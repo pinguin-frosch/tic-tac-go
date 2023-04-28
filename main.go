85401b393ec300ad2d197fe20d32af9c9aaffde6
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-04-28 15:54:09 -0400

    Métodos para determinar ganador y celda válida

--- main.go
+++ main.go
@@ -21,11 +21,64 @@ type Player struct {
 }
 
 type Board struct {
-	tiles [3][3]string
+	tiles     [3][3]string
+	usedTiles uint
+}
+
+func (b Board) validTile(row int, col int) bool {
+	if row >= len(b.tiles) || col >= len(b.tiles[0]) {
+		return false
+	}
+
+	if b.tiles[row][col] != "" {
+		return false
+	}
+
+	return true
+}
+
+func (b Board) checkWin(row int, col int, symbol string) int {
+	directions := map[string]int{"v": 0, "h": 0, "r": 0, "l": 0}
+	for i := 0; i < 3; i++ {
+		if b.tiles[i][col] != symbol {
+			break
+		}
+		directions["v"]++
+	}
+	for j := 0; j < 3; j++ {
+		if b.tiles[row][j] != symbol {
+			break
+		}
+		directions["h"]++
+	}
+	for k := 0; k < 3; k++ {
+		if b.tiles[k][k] != symbol {
+			break
+		}
+		directions["l"]++
+	}
+	for l := 0; l < 3; l++ {
+		if b.tiles[2-l][l] != symbol {
+			break
+		}
+		directions["r"]++
+	}
+
+	for k := range directions {
+		if directions[k] == 3 {
+			return 1
+		}
+	}
+
+	if b.usedTiles < 9 {
+		return 0
+	} else {
+		return -1
+	}
 }
 
 func (b Board) printBoard() {
-	fmt.Println("+-+-+-+")
+	fmt.Println("\n+-+-+-+")
 	for i := range b.tiles {
 		for j := range b.tiles[i] {
 			symbol := b.tiles[i][j]
@@ -37,4 +90,5 @@ func (b Board) printBoard() {
 		}
 		fmt.Println("|\n+-+-+-+")
 	}
+	fmt.Println()
 }
