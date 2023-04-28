8ca456267cc1f1f491dd2eba56774a0e9cebccdc
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-04-28 12:55:50 -0400

    Struct para el tablero y método para imprimirlo

--- main.go
+++ main.go
@@ -6,4 +6,25 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+	var board Board
+	board.printBoard()
+}
+
+type Board struct {
+	tiles [3][3]string
+}
+
+func (b Board) printBoard() {
+	fmt.Println("+-+-+-+")
+	for i := range b.tiles {
+		for j := range b.tiles[i] {
+			symbol := b.tiles[i][j]
+			if symbol == "" {
+				fmt.Print("|", " ")
+			} else {
+				fmt.Print("|", symbol)
+			}
+		}
+		fmt.Println("|\n+-+-+-+")
+	}
 }
